055a4e4be357286505274dd45b61d1a2afd58ec2
IdanAlbilia <54328209+IdanAlbilia@users.noreply.github.com>
2019-10-05 18:29:57 +0300

    Add files via upload

---
+++ ass0/Ex1.java
@@ -0,0 +1,12 @@
+/* Author: Idan Albilia 311375612!
+*  This program prints "Welcome to 37211111 - Introduction To Programming Science course" 
+*/
+
+
+ public class Ex1
+ {
+	public static void main(String[] args)
+	{	
+		System.out.println("Welcome to 37211111 - Introduction To Programming Science course");
+	}
+}
